ead1d4773b1a04d3456992ddca24c0730c435288
Dumi Loghin <dumiloghin@gmail.com>
2025-02-28 14:26:25 +0800

    update config

--- devnet/contracts/hardhat.config.js
+++ devnet/contracts/hardhat.config.js
@@ -10,6 +10,10 @@ module.exports = {
     devnet:{
       url: "http:127.0.0.1:8545",
       accounts: ["0x26e86e45f6fc45ec6e2ecd128cec80fa1d1505e5507dcd2ae58c3130a7a97b48", "0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8"],
+    },
+    devnet2:{
+      url: "http:127.0.0.1:8123",
+      accounts: ["0x26e86e45f6fc45ec6e2ecd128cec80fa1d1505e5507dcd2ae58c3130a7a97b48", "0x45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8"],
     }
   }
 };
